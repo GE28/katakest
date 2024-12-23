34a05bd0672aa067f1a69141184390ac837899c4
GE28 <44209041+GE28@users.noreply.github.com>
2024-12-23 11:40:12 -0300

    feat: add answer with blocks logic

---
+++ src/logic/useAnswerLogic/index.ts
@@ -0,0 +1,49 @@
+import { createSignal } from "solid-js";
+import AnswerBlock from "@logic/AnswerBlock";
+
+function useAnswerLogic() {
+  const [selectedAnswers, setSelectedAnswers] = createSignal<AnswerBlock[]>([]);
+  // because the mapped blocks at main screen are not reactive, we cannot use order as a block property
+  const blockOrderMap = new Map<string, number>();
+
+  const getCurrentAnswer = () => {
+    return selectedAnswers().map((a) => a.getJPchar());
+  };
+
+  const getCurrentPlainAnswer = () => {
+    return selectedAnswers()
+      .map((a) => a.getRomaji())
+      .join("");
+  };
+
+  const selectAnswer = (answer: AnswerBlock) => {
+    setSelectedAnswers([...selectedAnswers(), answer]);
+    blockOrderMap.set(answer.getId(), selectedAnswers().length);
+  };
+
+  const deselectAnswer = (answer: AnswerBlock) => {
+    setSelectedAnswers(selectedAnswers().filter((a) => a !== answer));
+    setBlocksOrder();
+  };
+
+  const setBlocksOrder = () => {
+    selectedAnswers().forEach((a, i) => {
+      blockOrderMap.set(a.getId(), i + 1);
+    });
+  };
+
+  const getBlockOrder = (block: AnswerBlock) => {
+    return blockOrderMap.get(block.getId()) || 0;
+  };
+
+  return {
+    getCurrentAnswer,
+    getCurrentPlainAnswer,
+    getBlockOrder,
+    selectedAnswers,
+    selectAnswer,
+    deselectAnswer,
+  };
+}
+
+export default useAnswerLogic;
